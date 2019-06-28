7750c015d826cc11a59d97427f1cba20057b7ed4
chengyang.dcy <chengyang.dcy@alibaba-inc.com>
2019-06-28 15:59:40 +0800

    make demo can work

--- python/tensorflow_apigateway/demo.py
+++ python/tensorflow_apigateway/demo.py
@@ -27,14 +27,14 @@ def demo():
     # 以下三行的信息均可在EAS管控台的服务列表，点击服务名称查看
     app_key = ''
     app_secret = ''
-    url = 'http://1c3b37ea83c047efa0dc6df0cacb70d3-cn-shanghai.alicloudapi.com/EAPI_249821151493486525_mnist_saved_model_example'
+    url = ''
 
-    # 请求数据可以参考阿里云EAS官方文档「通用processor服务请求数据构造」章节，根据自己的模型类型进行请求数据构造并序列化之后再进行请求
     request = tf_predict_pb2.PredictRequest()
-    request.signature_name = 'predict_images'
-    request.inputs['images'].dtype = tf_predict_pb2.DT_FLOAT  # 与模型中inputs.type对应
-    request.inputs['images'].array_shape.dim.extend([1, 784])  # 与模型中inputs.shape对应
-    request.inputs['images'].float_val.extend([0] * 784)  # data
+    # request.signature_name = 'predict'
+    request.inputs['input_1'].dtype = tf_predict_pb2.DT_FLOAT  
+    request.inputs['input_1'].array_shape.dim.extend([1,224,224,3]) 
+    request.inputs['input_1'].float_val.extend([0] * 224*224*3)    
+
 
     # 将pb序列化成string进行传输
     request_data = request.SerializeToString()
